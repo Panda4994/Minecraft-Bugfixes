2cac6d728f1f3a8b6aa5bd11e59cd267f1bfec58
Panda4994 <allid@live.de>
2015-09-05 03:21:13 +0200

    Create BlockPistonMoving.java

---
+++ BlockPistonMoving.java
@@ -0,0 +1,25 @@
+package net.minecraft.block;
+
+public class BlockPistonMoving extends BlockContainer {
+   public static final PropertyDirection field_176426_a = BlockPistonExtension.field_176326_a;
+   public static final PropertyEnum field_176425_b = BlockPistonExtension.field_176325_b;
+   private static final String __OBFID = "CL_00000368";
+
+   // Removed methods that don't change
+
+   public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos, IBlockState state) {
+      TileEntityPiston var4 = this.func_176422_e(worldIn, pos);
+      if(var4 == null) {
+         return null;
+      } else {
+         float var5 = var4.func_145860_a(0.0F);
+         if(var4.isExtending()) {
+            var5 = 1.0F - var5;
+         }
+
+         return this.func_176424_a(worldIn, pos, var4.func_174927_b(), var5, var4.func_174930_e());
+      }
+   }
+
+   // Removed methods that don't change
+}
