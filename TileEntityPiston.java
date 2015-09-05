dc46dd61eadc39729230cf6554c9adacd345ccdb
Panda4994 <allid@live.de>
2015-09-05 03:13:53 +0200

    Fixed entity movement for retracting pistons

--- TileEntityPiston.java
+++ TileEntityPiston.java
@@ -13,16 +13,33 @@ public class TileEntityPiston extends TileEntity implements
 
 	// Removed some methods that don't change
 
-	private void func_145863_a(float p_145863_1_, float p_145863_2_) {
+	/**
+	 * ADDED: Returns the actual direction the piston pushes in
+	 * 
+	 * @return facing to the direction the piston pushes in
+	 */
+	public EnumFacing getPushFacing() {
 		if (this.extending) {
-			p_145863_1_ = 1.0F - p_145863_1_;
+			return this.field_174931_f;
 		} else {
-			--p_145863_1_;
+			return this.field_174931_f.getOpposite();
 		}
+	}
+
+	// Method that moves the entities
+	private void func_145863_a(float p_145863_1_, float p_145863_2_) {
+		// Kept adjusting the first parameter as this was done for extending
+		// before as well
+		p_145863_1_ = 1.0F - p_145863_1_;
+		// Get actual push direction and replaced all calls of the facing in
+		// this method with this facing
+		// This way it will work exactly the same without having to mess with
+		// all the single values
+		EnumFacing pushFacing = getPushFacing();
 
 		AxisAlignedBB var3 = Blocks.piston_extension.func_176424_a(
 				this.worldObj, this.pos, this.field_174932_a, p_145863_1_,
-				this.field_174931_f);
+				pushFacing);
 		if (var3 != null) {
 			List var4 = this.worldObj.getEntitiesWithinAABBExcludingEntity(
 					(Entity) null, var3);
@@ -32,31 +49,32 @@ public class TileEntityPiston extends TileEntity implements
 
 				while (var5.hasNext()) {
 					Entity var6 = (Entity) var5.next();
-					if (this.field_174932_a.getBlock() == Blocks.slime_block
-							&& this.extending) {
-						switch (TileEntityPiston.SwitchAxis.field_177248_a[this.field_174931_f
+					// REMOVED: "&& this.extending", to make slime blocks bounce
+					// when the piston is retracting
+					if (this.field_174932_a.getBlock() == Blocks.slime_block) {
+						switch (TileEntityPiston.SwitchAxis.field_177248_a[pushFacing
 								.getAxis().ordinal()]) {
 						case 1:
-							var6.motionX = (double) this.field_174931_f
+							var6.motionX = (double) pushFacing
 									.getFrontOffsetX();
 							break;
 						case 2:
-							var6.motionY = (double) this.field_174931_f
+							var6.motionY = (double) pushFacing
 									.getFrontOffsetY();
 							break;
 						case 3:
-							var6.motionZ = (double) this.field_174931_f
+							var6.motionZ = (double) pushFacing
 									.getFrontOffsetZ();
 						}
-					} else {
-						var6.moveEntity(
-								(double) (p_145863_2_ * (float) this.field_174931_f
-										.getFrontOffsetX()),
-								(double) (p_145863_2_ * (float) this.field_174931_f
-										.getFrontOffsetY()),
-								(double) (p_145863_2_ * (float) this.field_174931_f
-										.getFrontOffsetZ()));
 					}
+					// Made it move the entity also when it's a slime block. See
+					// MC-??? for additional information about that.
+					var6.moveEntity((double) (p_145863_2_ * (float) pushFacing
+							.getFrontOffsetX()),
+							(double) (p_145863_2_ * (float) pushFacing
+									.getFrontOffsetY()),
+							(double) (p_145863_2_ * (float) pushFacing
+									.getFrontOffsetZ()));
 				}
 
 				this.field_174933_k.clear();
@@ -83,10 +101,10 @@ public class TileEntityPiston extends TileEntity implements
 				this.progress = 1.0F;
 			}
 
-			if (this.extending) {
-				this.func_145863_a(this.progress, this.progress
-						- this.lastProgress + 0.0625F);
-			}
+			// Call the movement method always, no matter if extending or
+			// retracting.
+			this.func_145863_a(this.progress, this.progress - this.lastProgress
+					+ 0.0625F);
 		}
 	}
 
