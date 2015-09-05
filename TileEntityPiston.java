0a76965c658d9b7287710652900eae5ac760e493
Panda4994 <allid@live.de>
2015-09-05 03:05:58 +0200

    Fixed entity movement for retracting pistons

--- TileEntityPiston.java
+++ TileEntityPiston.java
@@ -12,41 +12,56 @@ public class TileEntityPiston extends TileEntity implements IUpdatePlayerListBox
 
    // Removed some methods that don't change
 
+   /** ADDED:
+    * Returns the actual direction the piston pushes in
+    * 
+    * @return	facing to the direction the piston pushes in
+    */
+   public EnumFacing getPushFacing() {
+	   if (this.extending) {
+		   return this.field_174931_f;
+	   } else {
+		   return this.field_174931_f.getOpposite();
+	   }
+   }
+   
+   // Method that moves the entities
    private void func_145863_a(float p_145863_1_, float p_145863_2_) {
-      if(this.extending) {
-         p_145863_1_ = 1.0F - p_145863_1_;
-      } else {
-         --p_145863_1_;
-      }
+	   // Kept adjusting the first parameter as this was done for extending before as well
+	   p_145863_1_ = 1.0F - p_145863_1_;
+	   // Get actual push direction and replaced all calls of the facing in this method with this facing
+	   // This way it will work exactly the same without having to mess with all the single values
+	   EnumFacing pushFacing = getPushFacing();
+	   
+	   AxisAlignedBB var3 = Blocks.piston_extension.func_176424_a(this.worldObj, this.pos, this.field_174932_a, p_145863_1_, pushFacing);
+	   if(var3 != null) {
+		   List var4 = this.worldObj.getEntitiesWithinAABBExcludingEntity((Entity)null, var3);
+	       if(!var4.isEmpty()) {
+	    	   this.field_174933_k.addAll(var4);
+	           Iterator var5 = this.field_174933_k.iterator();
 
-      AxisAlignedBB var3 = Blocks.piston_extension.func_176424_a(this.worldObj, this.pos, this.field_174932_a, p_145863_1_, this.field_174931_f);
-      if(var3 != null) {
-         List var4 = this.worldObj.getEntitiesWithinAABBExcludingEntity((Entity)null, var3);
-         if(!var4.isEmpty()) {
-            this.field_174933_k.addAll(var4);
-            Iterator var5 = this.field_174933_k.iterator();
+	           while(var5.hasNext()) {
+	        	   Entity var6 = (Entity)var5.next();
+	        	   // REMOVED: "&& this.extending", to make slime blocks bounce when the piston is retracting
+	               if(this.field_174932_a.getBlock() == Blocks.slime_block) {
+	                  switch(TileEntityPiston.SwitchAxis.field_177248_a[pushFacing.getAxis().ordinal()]) {
+	                  case 1:
+	                     var6.motionX = (double)pushFacing.getFrontOffsetX();
+	                     break;
+	                  case 2:
+	                     var6.motionY = (double)pushFacing.getFrontOffsetY();
+	                     break;
+	                  case 3:
+	                     var6.motionZ = (double)pushFacing.getFrontOffsetZ();
+	                  }
+	               }
+	               // Made it move the entity also when it's a slime block. See MC-??? for additional information about that.
+	               var6.moveEntity((double)(p_145863_2_ * (float)pushFacing.getFrontOffsetX()), (double)(p_145863_2_ * (float)pushFacing.getFrontOffsetY()), (double)(p_145863_2_ * (float)pushFacing.getFrontOffsetZ()));
+	           }
 
-            while(var5.hasNext()) {
-               Entity var6 = (Entity)var5.next();
-               if(this.field_174932_a.getBlock() == Blocks.slime_block && this.extending) {
-                  switch(TileEntityPiston.SwitchAxis.field_177248_a[this.field_174931_f.getAxis().ordinal()]) {
-                  case 1:
-                     var6.motionX = (double)this.field_174931_f.getFrontOffsetX();
-                     break;
-                  case 2:
-                     var6.motionY = (double)this.field_174931_f.getFrontOffsetY();
-                     break;
-                  case 3:
-                     var6.motionZ = (double)this.field_174931_f.getFrontOffsetZ();
-                  }
-               } else {
-                  var6.moveEntity((double)(p_145863_2_ * (float)this.field_174931_f.getFrontOffsetX()), (double)(p_145863_2_ * (float)this.field_174931_f.getFrontOffsetY()), (double)(p_145863_2_ * (float)this.field_174931_f.getFrontOffsetZ()));
-               }
-            }
-
-            this.field_174933_k.clear();
-         }
-      }
+	           this.field_174933_k.clear();
+	       }
+	   }
    }
 
    // Removed some methods that don't change
@@ -67,9 +82,8 @@ public class TileEntityPiston extends TileEntity implements IUpdatePlayerListBox
             this.progress = 1.0F;
          }
 
-         if(this.extending) {
-            this.func_145863_a(this.progress, this.progress - this.lastProgress + 0.0625F);
-         }
+         // Call the movement method always, no matter if extending or retracting.
+				 this.func_145863_a(this.progress, this.progress - this.lastProgress + 0.0625F);
       }
    }
 
